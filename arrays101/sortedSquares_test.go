a2f7b6804095ad9ef9654add8a44ea009ccf158a
yuri.liang <yuri.liang@ericsson.com>
2021-09-07 10:23:07 +0800

    fix testcase

--- arrays101/sortedSquares_test.go
+++ arrays101/sortedSquares_test.go
@@ -6,7 +6,7 @@ func TestSortedSquares(t *testing.T) {
 	nums := []int{-7, -3, 2, 3, 11}
 	want := []int{4, 9, 9, 49, 121}
 	res := sortedSquares(nums)
-	if Equal(res, want) {
+	if !Equal(res, want) {
 		t.Fatalf("testing sorted squares with %v, want: %v, but return: %v", nums, want, res)
 	}
 }
@@ -15,8 +15,8 @@ func Equal(a, b []int) bool {
 	if len(a) != len(b) {
 		return false
 	}
-	for i, v := range a {
-		if v != b[i] {
+	for idx, val := range a {
+		if val != b[idx] {
 			return false
 		}
 	}
