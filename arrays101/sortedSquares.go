3e60f570273a489be7a05946999baeccf610f6dc
yuri.liang <yuri.liang@ericsson.com>
2021-09-07 18:09:55 +0800

    TODO: continue to update

--- arrays101/sortedSquares.go
+++ arrays101/sortedSquares.go
@@ -5,9 +5,12 @@ func sortedSquares(nums []int) []int {
 	for _, num := range nums {
 		square_nums = append(square_nums, num*num)
 	}
-	for i := len(square_nums) - 1; i >= 0; i-- {
-		if square_nums[i] < square_nums[0] {
-			square_nums[i], square_nums[0] = square_nums[0], square_nums[i]
+	for i, j := 0, len(square_nums)-1; j == i; {
+		if square_nums[j] < square_nums[i] {
+			square_nums[i], square_nums[j] = square_nums[j], square_nums[i]
+			j -= 1
+		} else {
+			j -= 1
 		}
 	}
 	return square_nums
